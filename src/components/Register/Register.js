5f06ab0dcbd30b17a9b577dac7b6a5e4e2bbf7a9
hrvoje-rama <hrvojeramljak@gmail.com>
2021-03-17 21:28:15 +0100

    adding node

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://powerful-lake-28992.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
